6c34b21586617a658c816d16d8f85f758f982497
Amrzs0971 <2637827727@qq.com>
2023-10-29 19:19:36 +0800

    Rollback changes

--- src/itdelatrisu/opsu/options/Options.java
+++ src/itdelatrisu/opsu/options/Options.java
@@ -1521,7 +1521,7 @@ public class Options {
 		else {
 			// load the skin
 			skin = SkinLoader.loadSkin(skinDir);
-			String info = skin.name + " (" + skin.author + ")";
+			String info = skin.getName() + " (" + skin.getAuthor() + ")";
 			ErrorHandler.error(String.format("You are using the skin '%s'.", info), null, false);
 			// Show the real name and author in skin.ini
 			ResourceLoader.addResourceLocation(new FileSystemLocation(skinDir));
