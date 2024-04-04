774455fc9e734cd1cfd569d993d75c48c09c5bf2
复予 <1530207757@qq.com>
2024-04-04 13:02:21 +0800

    Comment out unused I18n functions

--- src/itdelatrisu/opsu/I18n.java
+++ src/itdelatrisu/opsu/I18n.java
@@ -1,34 +1,69 @@
+/*
+ * opsu! - an open-source osu! client
+ * Copyright (C) 2024 CloneWith
+ *
+ * opsu! is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * opsu! is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with opsu!.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package itdelatrisu.opsu;
 
-import gnu.gettext.GettextResource;
-
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
  * Simple I18n implementation.
- * Source: https://github.com/sba1/simpledance/blob
+ *
+ * @see https://github.com/sba1/simpledance
  */
 public class I18n {
-	static private ResourceBundle locres;
+	static private Properties prop;
 	static private Boolean locap = false;
 
-	static final private void LocaleInit() {
-		try {
-			locres = GettextResource.getBundle("Opsu");
-		} catch (Exception e) {};
+	static private void LocaleInit() {
+		/*
+		Locale loc = Locale.getDefault();
+		String lang = loc.getLanguage();
+		String co = loc.getCountry();
+		String fname = "i18n/" + lang + "_" + co + ".properties";
+		prop = new Properties();
+		try{
+			InputStream inputStream = I18n.class.getClassLoader().getResourceAsStream(fname);
+			prop.load(inputStream);
+		}
+		catch (IOException e) {} */
 		locap = true;
 	};
 
 	/**
 	 * Returns a translated string.
+	 *
 	 * @param str string key
 	 * @return The translated string by `gettext`
 	 */
 	static final public String t(String str) {
 		if (!locap)
 			LocaleInit();
-		if (locres == null)
-			return str;
-		return GettextResource.gettext(locres, str);
+		return str;
+		// if (prop == null)
+		// 	return str;
+		// return prop.getProperty(str);
+		// return locres.getString(str);
+		// return GettextResource.gettext(locres, str);
 	}
 }
